cbd08c41b680de29c4d6745df5e12a49c4d410a0
AleksaBastinac06 <144840902+AleksaBastinac06@users.noreply.github.com>
2024-01-10 15:35:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//free weather api
+const e1 = document.querySelectorAll(".email1");
+const e2 = document.querySelectorAll(".email2");
+const handleButtonClick = (e1) => {
+  alert(
+    "Prijavili ste se na newsletter!Dobijaćete pravovremene informacije sa sajta!"
+  );
+};
